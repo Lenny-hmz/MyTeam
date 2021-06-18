df5320f408b2331cce8dff18dea3102533307664
schueler <schueler@ubuntu>
2021-06-18 11:22:56 +0200

    init MyTeam

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,7 +7,7 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic ", "Defense");
 		System.out.println("== " + player);
-		Player player = new Player(4, "Hinteregger", "Defense");
+		player = new Player(4, "Hinteregger", "Defense");
 		System.out.println("== " + player);
 	}
 }
